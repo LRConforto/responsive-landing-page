9ece1ca12307c1b5908040c40a803a1f6f4026ef
Lucas Rodrigues <lrconforto@hotmail.com>
2025-06-19 07:01:29 -0300

    Adicionado avaliações, footer e algumas outras alterações.

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -6,7 +6,22 @@
     $('#mobile-btn').on('click', function (){
          $('#mobile-menu').toggleClass('active');
          $('#mobile-btn').find('i').toggleClass('fa-x');
+    });
+
+    const sections = $('section');
+    const navItems = $('.nav-item');
+
+    $(window).on('scroll', function(){ 
+        const header = $('header');
+        const posicaoScroll = $(window).scrollTop() - header.outerHeight();
+
+        if (posicaoScroll <= 0) { 
+            header.css('box-shadow', 'none') 
+        } else { 
+            header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.1)');
+        }
     })
+
 });
 
 /*   
