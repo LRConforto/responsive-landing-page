ae3166f1ab22ab948810228d1ac354f0e59f5d95
Lucas Rodrigues <lrconforto@hotmail.com>
2025-06-19 22:19:21 -0300

    Adicionado Scrollreveal no conteudo da página, e outras alterações com Jquery

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,6 +1,4 @@
- //Falando por JQUERY iniciar apenas depois de carregar toda a página
- //adicionado o evento onclick para o mobile-btn
- //quando for clicado no mobile-btn ele vai
+ //Iniciar JQUERY apenas depois de carregar toda a página.
 
  $(document).ready(function() {
     $('#mobile-btn').on('click', function (){
@@ -15,25 +13,55 @@
         const header = $('header');
         const posicaoScroll = $(window).scrollTop() - header.outerHeight();
 
+        let activeSectionIndex = 0; 
+
+
         if (posicaoScroll <= 0) { 
-            header.css('box-shadow', 'none') 
+            header.css('box-shadow', 'none'); 
         } else { 
             header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.1)');
         }
-    })
 
+        sections.each(function (i){
+            
+            const section = $(this);
+            const sectionTop = section.offset().top - 96;
+            const sectionBottom = sectionTop + section.outerHeight();
+
+            if(posicaoScroll >= sectionTop && posicaoScroll < sectionBottom){
+                activeSectionIndex = i;
+                return false;
+            }
+        })
+
+        navItems.removeClass('active');    
+        $(navItems[activeSectionIndex]).addClass('active');
+    })
 });
 
-/*   
-Com JS puro.
+//ScrollReveal
 
-function clickMenu() {
-        let mmenu = document.querySelector('#mobile-menu'); 
-        if (mmenu.style.display == 'none') { 
-           mmenu.style.display = 'block'
-        } else { 
-            mmenu.style.display = 'none'
-        }
-} */
+ ScrollReveal().reveal('#call-to-action, #dishes', { 
+    origin: 'left',
+    duration: 2000,
+    distance: '20%'
+ });
 
-        
+  ScrollReveal().reveal('#banner', { 
+    origin: 'top',
+    duration: 1800,
+    distance: '20%',
+ });
+
+
+ ScrollReveal().reveal('#depoimento-chef', { 
+    origin: 'left',
+    duration: 1500,
+    distance: '20%',
+ });
+
+ScrollReveal().reveal('#feedbacks', { 
+    origin: 'right',
+    duration: 1500,
+    distance: '20%'
+});
